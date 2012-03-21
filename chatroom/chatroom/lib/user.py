70712dffd168eaa8da32d51c766ff6ff1bbacdcc
Kai Liu <hfevers@gmail.com>
2012-03-21 19:23:36 -0400

    add user class

---
+++ chatroom/chatroom/lib/user.py
@@ -0,0 +1,9 @@
+
+class User:
+    def __init__(self, userId, name):
+        self.userId = userId
+        self.name = name
+    def __str__(self):
+        return "{'User Id': %s, 'Name': '%s'}" % \
+                (self.userId, self.name)
+
