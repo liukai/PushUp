fa87506c114d5b2a76ea32505e4c31bf756a8105
Kai Liu <hfevers@gmail.com>
2012-03-30 13:13:11 -0400

    remove redis

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,9 +1,5 @@
 PYLONS_PID_FILE=pylons.pid
 
-# Start the redis
-echo "Starting redis..."
-redis-server > redis.log &
-
 # Start the pylons 
 echo "Starting chatroom..."
 chatroom.sh start
--- scripts/stop-all.sh
+++ scripts/stop-all.sh
@@ -1,6 +1,3 @@
-echo "Shutting down the chatroom"
-chatroom.sh stop
-
 echo "Shutting down redis"
 redis-cli shutdown
 
